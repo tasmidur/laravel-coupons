dd2dae3c7cf36398a5b0af77e3e597daad0b022c
Md Tasmidur Rahman <tasmidurrahman@gmail.com>
2022-07-21 04:06:02 +0600

    Project is done

---
+++ src/Traits/CouponTrait.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Tasmidur\Coupon\Traits;
+
+use Carbon\Carbon;
+use Exception;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Support\Facades\Log;
+use Tasmidur\Coupon\Facades\Coupons;
+use Tasmidur\Coupon\Models\Coupon;
+
+trait CouponTrait
+{
+
+    /**
+     * @throws Exception
+     */
+    public function applyCoupon(string $code)
+    {
+        $coupon = Coupons::check($code);
+        if ($coupon->isExpired()) {
+            throw new Exception("The coupon is already expire now");
+        }
+        $this->coupons()->attach($coupon, [
+            'applied_at' => Carbon::now()
+        ]);
+        return $coupon;
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function applyUniqueCoupon(string $code)
+    {
+        $coupon = Coupons::check($code);
+        Log::info($this->coupons()->get());
+        if ($this->coupons()->count() > 0) {
+            throw new Exception("The coupon is already applied");
+        }
+
+        if ($coupon->isExpired()) {
+            throw new Exception("The coupon is already expire now");
+        }
+        $this->coupons()->attach($coupon, [
+            'applied_at' => Carbon::now()
+        ]);
+        return $coupon;
+    }
+
+    /**
+     * @return BelongsToMany
+     */
+    public function coupons(): BelongsToMany
+    {
+        return $this->belongsToMany(Coupon::class, config('coupon.relation_table', "coupon_applied"), 'coupon_id', "apply_for_id")->withPivot('applied_at');
+    }
+
+    /**
+     * @return BelongsToMany
+     */
+    public function couponByCouponId(int $id): BelongsToMany
+    {
+        return $this->belongsToMany(Coupon::class, config('coupon.relation_table', "coupon_applied"), 'coupon_id', "apply_for_id")
+            ->wherePivot('coupon_id', $id)
+            ->withPivot('applied_at');
+    }
+}
